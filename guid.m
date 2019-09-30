64baa56b371e06429fbab6b223a14a31ba0576ae
brunozn <46513538+brunozn@users.noreply.github.com>
2019-09-30 13:50:49 -0300

    Projeto Inicial

---
+++ guid.m
@@ -0,0 +1,141 @@
+function varargout = guide(varargin)
+% GUIDE MATLAB code for guide.fig
+%      GUIDE, by itself, creates a new GUIDE or raises the existing
+%      singleton*.
+%
+%      H = GUIDE returns the handle to a new GUIDE or the handle to
+%      the existing singleton*.
+%
+%      GUIDE('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUIDE.M with the given input arguments.
+%
+%      GUIDE('Property','Value',...) creates a new GUIDE or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before guide_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to guide_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help guide
+
+% Last Modified by GUIDE v2.5 30-Sep-2019 11:21:23
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @guide_OpeningFcn, ...
+                   'gui_OutputFcn',  @guide_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before guide is made visible.
+function guide_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to guide (see VARARGIN)
+
+% Choose default command line output for guide
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes guide wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = guide_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in botaoReset.
+function N_Callback(hObject, eventdata, handles)
+% hObject    handle to N (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of N as text
+%        str2double(get(hObject,'String')) returns contents of N as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function N_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to N (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function D_Callback(hObject, eventdata, handles)
+% hObject    handle to D (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of D as text
+%        str2double(get(hObject,'String')) returns contents of D as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function D_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to D (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in butaocalc.
+function butaocalc_Callback(hObject, eventdata, handles)
+text = get(handles.N, 'String');
+convertString_int = str2num(text);
+raizes_numerador = roots(convertString_int);
+set(handles.E, 'String', raizes_numerador);
+plot(raizes_numerador,'O')
+hold on
+text1 = get(handles.D, 'String');
+conversor = str2num(text1)
+raizes_denominador = roots(conversor);
+set(handles.E2, 'String', raizes_denominador);
+plot(raizes_denominador,'X')
+
+% --- Executes on button press in botaoresetar.
+function botaoresetar_Callback(hObject, eventdata, handles)
+set(handles.E, 'String', '');
+set(handles.E2, 'String', '');
+set(handles.D, 'String', '');
+set(handles.N, 'String', '');
